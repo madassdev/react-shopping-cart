1256a7de75496dd2a171e27713a120c849042462
madassdev <favescsskr@gmail.com>
2021-06-21 11:55:54 +0100

    Product component done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="#">Shopping Cart</a>
-      </header>
-      <main>
-        Shopping list
-      </main>
-      <footer>
-        Copyright 2021 &copy; <a href="http://madassdev.com">Madassdev</a>
-      </footer>
-    </div>
-  );
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="#">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>
+          Copyright 2021 &copy; <a href="http://madassdev.com">Madassdev</a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
